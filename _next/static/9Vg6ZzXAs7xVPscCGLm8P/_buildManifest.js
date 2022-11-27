7cc6c2b869f7cd7adca0e70f6e780b7aec5a1864
hsjobeki <hsjobeki@users.noreply.github.com>
2022-11-27 13:44:07 +0000

    deploy: d4ce8d7f219a951cd4535f9faff29f55d5ee68a2

--- _next/static/9Vg6ZzXAs7xVPscCGLm8P/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/427-0271489c571a7857.js","static/chunks/pages/index-f1d246f94e2eb2b3.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/YmWNS-qVbKqKoIdewNDi-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/427-63e1400e6eddc060.js","static/chunks/pages/index-d27906d84a2791bd.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
