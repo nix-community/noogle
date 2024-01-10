4e54cf59890987d2846aeebe7250b8d71965592a
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-01-10 13:14:42 +0100

    fix autolinks for ' (Prime) cases

--- website/src/components/searchInput/searchInput.tsx
+++ website/src/components/searchInput/searchInput.tsx
@@ -81,7 +81,7 @@ export function SearchInput(props: SearchInputProps) {
           if (reason === "reset" && e) {
             console.log({ e, value, reason });
             if (value) {
-              router.push(`/f/${value.split(".").join("/")}`);
+              router.push(`/f/${value.split(".").join("/")}`.split(" ")[0]);
             } else {
               handleSubmit(value);
             }
