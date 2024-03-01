2a3c23b8d2e062a2a5025dba77d46bca6d602c90
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-03-01 12:43:33 +0100

    add experimental builtins inherited by flake & commands features

--- pasta/default.nix
+++ pasta/default.nix
@@ -4,7 +4,7 @@ pkgs.stdenv.mkDerivation {
   src = ./src;
   nativeBuildInputs = [ nix ];
   buildPhase = ''
-    nix-instantiate --eval --strict --json --store $PWD --show-trace \
+    nix-instantiate --extra-experimental-features 'nix-command flakes' --eval --strict --json --store $PWD --show-trace \
       eval.nix --arg 'pkgs' 'import ${nixpkgs} {}' --arg 'repo' '${nixpkgs.outPath}' -A all \
       > $out
   '';
