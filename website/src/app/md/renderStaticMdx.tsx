94a2980f0c8d81daa22ccc872005e5a5c2489173
Johannes Kirschbauer <hsjobeki@gmail.com>
2026-06-15 10:18:51 +0200

    chore: remove deprecated type imports

--- website/src/app/md/renderStaticMdx.tsx
+++ website/src/app/md/renderStaticMdx.tsx
@@ -3,16 +3,15 @@ import { mdxRenderOptions } from "@/utils";
 import fs from "fs";
 
 export async function renderStaticMdx(url: string) {
-  const source = fs.readFileSync(
-    url,
-    "utf-8"
-  );
+  const source = fs.readFileSync(url, "utf-8");
 
   const mdxOptions = { ...mdxRenderOptions, format: "mdx" as const };
 
   const { content } = await compileMDX({
     source,
     options: {
+      // @ts-expect-error: since we upgraded next-mdx it doesnt export the types anymore
+      // And i am too lazy to figure out their new rendering pipeline :D
       mdxOptions,
     },
   });
